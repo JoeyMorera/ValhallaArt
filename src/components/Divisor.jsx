4d9fee0c838721faf5391e9881fa74fbdb410265
Jocsan Morera <joey_morera13@hotmail.com>
2023-04-03 18:06:04 -0600

    update divisor.jsx

--- src/components/Divisor.jsx
+++ src/components/Divisor.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import trees from '../../src/assets/bg/Trees.png'
+import mont from '../../src/assets/bg/mont.png'
 
 const Divisor = () => {
   return (
     <>
         <div className="w-full h-20 sm:h-40 md:h-52 lg:h-72 xl:h-80 relative bg-fixed bg-cover bg-clip-border fondo">
-          <img src="../src/assets/bg/mont.png" alt="rio" className="bg-fixed absolute top-[-30px] object-cover" />
-          <img src="../src/assets/bg/Trees.png" alt="rio" className="bg-cover bg-clip-border absolute top-[-50px] bottom-0 object-cover" />
+          <img src={mont} alt="rio" className="bg-fixed absolute top-[-30px] object-cover" />
+          <img src={trees} alt="rio" className="bg-cover bg-clip-border absolute top-[-50px] bottom-0 object-cover" />
         </div>
     </>
   )
